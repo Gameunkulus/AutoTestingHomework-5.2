93b64dc09a3cecb7b437ec108ddead0975ddfd3e
Gameunkulus <bugr24-8@mail.ru>
2023-08-13 20:02:34 +0300

    program redacted 2

---
+++ src/test/java/CallBackTest.java
@@ -0,0 +1,47 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Condition;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.restassured.RestAssured.given;
+import static ru.netology.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.DataGenerator.getRandomPassword;
+
+public class CallBackTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should succesfulli login with active registered user")
+    void ShouldSuccessfulliLogin() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! \nНеверно указан логин или пароль"))
+                .shouldBe((Condition.visible));
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void ShouldGerErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! \nНеверно указан логин или пароль"))
+                .shouldBe((Condition.visible));
+    }
+
+}
